cadaa5d1b90a2cfe820baa526a79f2c0944f17f5
aka06nksha <akankshacirigiri@gmail.com>
2025-07-29 19:49:07 +0530

    pattern2

---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        pattern(4,0);
+    }
+    public static void pattern(int row,int col){
+         if(row==0){
+            return;
+         }
+         else if(col<row){
+            pattern(row,col+1);
+            System.out.print('*');
+         }
+         else{
+            pattern(row-1,0);
+             System.out.println();
+         }
+    }
+}
