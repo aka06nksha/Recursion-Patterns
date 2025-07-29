4777037fa3b2b70d8e853c6073e7b80539259c14
aka06nksha <akankshacirigiri@gmail.com>
2025-07-29 19:09:12 +0530

    patterns using recursion

---
+++ Pattern1.java
@@ -0,0 +1,18 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+           pattern(4,0);
+        }
+        public static void pattern(int row,int col){
+               if(row==0){
+                return ;
+               }
+               else if(col<row){
+                   System.out.print('*');
+                   pattern(row,col+1);
+               }
+               else{
+                 System.out.println();
+                 pattern(row-1,0);
+               }
+        }
+    }
