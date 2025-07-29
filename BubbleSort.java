a26495d0532fedf8a4f4bc3aedf24b4153976291
aka06nksha <akankshacirigiri@gmail.com>
2025-07-29 20:42:55 +0530

    bubblesortusingrecursion

---
+++ BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={5,8,6,4,2};
+        bubbleSortRecursion(arr, 4,0);
+        }
+    static void bubbleSortRecursion(int[] arr,int n,int i){
+        if(n==0){
+            return;
+        }
+        else if(i<n && arr[i]>arr[i+1]){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+            bubbleSortRecursion(arr, n,i+1);
+        }
+        else if(i<n && arr[i]<arr[i+1]){
+              bubbleSortRecursion(arr,n,i+1);
+        }
+        else{
+            bubbleSortRecursion(arr, n-1, 0);
+        }
+
+
+    }
+    
+}
